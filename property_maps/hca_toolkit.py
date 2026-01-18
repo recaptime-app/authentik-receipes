29f4d9d35f4ca783252e90c77acb81aeb848fcf4
Andrei Jiroh Halili <ajhalili2006@crew.recaptime.dev>
2026-01-18 22:19:04 +0800

    chore(property_maps/hca_toolkit): clarify comments and adjust return attributes
    Updated comments for clarity and modified return structure. To be deployed to Authentik soon.

--- property_maps/hca_toolkit.py
+++ property_maps/hca_toolkit.py
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: MPL-2.0
 # Author: Andrei Jiroh Halili (Recap Time Squad) <ajhalili2006@crew.recaptime.dev>
 # Description: Collect Hack Club Auth user info and save them as custom attributes for use in Authentik
+# Canonical source: https://github.com/recaptime-app/authentik-receipes/blob/main/property_maps/hca_toolkit.py
 
-# Retrieve Hack Club Slack user ID from OIDC claims
+# Retrieve Hack Club Slack user ID and verification details from OIDC claims provided by HC Auth
 # Additional scopes required: slack_id, verification_status
 hca_userinfo = {
   "user_id": info.get("sub"),
@@ -11,9 +12,13 @@ hca_userinfo = {
   "ysws_eligible": info.get("ysws_eligible", False),
 }
 
+# Then pass them to Authentik as custom user attributes.
 return {
+  #"hackclub_auth": hca_userinfo
   "attributes": {
     "auth.hackclub.com/userinfo": hca_userinfo,
+    "idproofstoolkit.recaptime.dev/hackclub_auth/user_id": hca_userinfo.user_id,
     "idproofstoolkit.recaptime.dev/hackclub_auth/verification_status": hca_userinfo.verification_status,
+    "idproofstoolkit.recaptime.dev/hackclub_auth/ysws_eligible": hca_userinfo.ysws_eligible
   }
 }
